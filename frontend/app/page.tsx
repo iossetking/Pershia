a10cb8d74f69e7071413f34f198b939a9e055d9d
Iossex.Dev <iossetking@gmail.com>
2026-03-10 08:37:45 -0600

    Added required npm packages.

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -8,14 +8,6 @@ export default function Home() {
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <Routes>
-          <Route element={<PublicLayout />}>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/about" element={<div>About Us</div>} />
-          </Route>
-          <Route path="/app" element={<AppLayout />}>
-            <Route index element={<Dashboard />} />
-            <Route path="tickets" element={<Tickets />} />
-          </Route>
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
